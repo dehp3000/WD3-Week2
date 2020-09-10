8121fa722b8a9d089cd2f41cf1220593252bf43e
dehp3000 <deh_p3000@yahoo.com>
2020-09-10 17:56:37 -0400

    added an image testing button event listeners

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,84 @@
-document.getElementById("gallery").innerHTML ='<h2>Carolina Parks <strong>Gallery</strong></h2>';
+document.querySelector("#gallery h2").innerHTML ='Carolina Parks <strong>Gallery</strong>';
+
+//create buttons for event listen to attach to.
+const prev = document.querySelector('.pagination li:first-of-type button');
+const next = document.querySelector('.pagination li:last-of-type button');
+
+//index varibale to keep track in array
+let index =0;
+
+//attach the event listeners to buttons
+next.addEventListener('click',nextImg);
+prev.addEventListener('click',prevImg);
+
+//next and previous functions
+function nextImg(){
+	//change index
+	alert('next image pressed');
+}
+
+function prevImg(){
+	//change index
+	alert('previous image pressed');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//create array and store paths
+let data = [
+	{
+		"image":"/images/image1.jpg",
+		"title":"title 1",
+		"desc":"Described image 1"
+	},
+	{
+		"image":"/images/image1.jpg",
+		"title":"title 2",
+		"desc":"Described image 2"
+	},
+	//3
+	{
+
+	},
+	//4
+	{
+
+	},
+	//5
+	{
+
+	}
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
